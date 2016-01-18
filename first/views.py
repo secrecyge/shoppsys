3d57e6ec76737a129e657ce193592b696f831626
secrecyge <secrecyge@126.com>
2016-01-18 10:32:03 +0800

    5

--- first/views.py
+++ first/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader,Context
+def index(request):
+    t=loader.get_template("index.html")
+    c=Context({"title":"django","name":"tom"})
+    return HttpResponse (t.render(c))
--- shopsys/urls.py
+++ shopsys/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'first/index/$','first.views.index'),
+    url(r'^first/index/$','first.views.index'),
 ]
